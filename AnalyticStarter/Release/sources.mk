419ac3d4a6051b53eebd84ad93b5445f4f434a0e
Anjana Tissera <anjanat@gmail.com>
2015-06-05 01:31:51 +0700

    AnalyticStarter .gitignore file changed

---
+++ AnalyticStarter/Release/sources.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/opencctv/util/log/colored \
+src/opencctv/util/log \
+src/opencctv/util \
+src/opencctv/mq \
+src/opencctv \
+src \
+src/analytic/xml \
+src/analytic/util \
+src/analytic \
+
