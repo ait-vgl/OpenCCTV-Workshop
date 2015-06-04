4554a957bce2a0350e61029bf520c22cfdd21d90
Anjana Tissera <anjanat@gmail.com>
2015-06-04 23:29:02 +0700

    OpenCCTVServer .gitignore file changed

---
+++ OpenCCTVServer/Release/sources.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/test/gateway \
+src/opencctv/util/serialization/gpb \
+src/opencctv/util/serialization \
+src/opencctv/util/log/colored \
+src/opencctv/util/log \
+src/opencctv/util/flow \
+src/opencctv/util \
+src/opencctv/mq \
+src/opencctv/dto \
+src/opencctv/db \
+src/opencctv \
+src \
+src/analytic/xml \
+src/analytic \
+
