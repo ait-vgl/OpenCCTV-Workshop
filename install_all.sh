c29446a27147741b316b0e28fb5486f5c3ffdae7
Lakindu Boteju <lakinduboteju@gmail.com>
2015-06-25 06:15:15 +0700

    installation script modified

--- install_all.sh
+++ install_all.sh
@@ -12,20 +12,20 @@ echo -e "${color_green}Installation directory created at $INSTALL_PATH.${color_r
 echo -e "${color_green}*Building OpenCCTV Server Starter...${color_reset}"
 cd OpenCCTVServer_Starter/Release/
 # make clean && make all
-make
+make all
 OPENCCTVSERVER_STARTER_PATH=${INSTALL_PATH%%/}/OpenCCTVServerStarter/
 mkdir -p ${OPENCCTVSERVER_STARTER_PATH}
-cp {OpenCCTVServer_Starter,opencctv-server.config,opencctv-server_starter.config} ${OPENCCTVSERVER_STARTER_PATH}
+cp OpenCCTVServer_Starter opencctv-server.config opencctv-server_starter.config ${OPENCCTVSERVER_STARTER_PATH}
 echo -e "${color_green}Done. OpenCCTV Server Starter installed at $OPENCCTVSERVER_STARTER_PATH.${color_reset}"
 
 # Installing OpenCCTVServer
 echo -e "${color_green}*Building OpenCCTV Server...${color_reset}"
 cd ../../OpenCCTVServer/Release/
 # make clean && make all
-make
+make all
 OPENCCTVSERVER_PATH=${INSTALL_PATH%%/}/OpenCCTVServer/
 mkdir -p ${OPENCCTVSERVER_PATH}
-cp {OpenCCTVServer} ${OPENCCTVSERVER_PATH}
+cp OpenCCTVServer ${OPENCCTVSERVER_PATH}
 # cp -a test/ ${OPENCCTVSERVER_PATH}
 echo -e "${color_green}Done. OpenCCTV Server installed at $OPENCCTVSERVER_PATH.${color_reset}"
 
@@ -33,17 +33,17 @@ echo -e "${color_green}Done. OpenCCTV Server installed at $OPENCCTVSERVER_PATH.$
 echo -e "${color_green}*Building Analytic Starter...${color_reset}"
 cd ../../AnalyticStarter/Release/
 # make clean && make all
-make
+make all
 STARTER_PATH=${INSTALL_PATH%%/}/AnalyticStarter
 mkdir -p ${STARTER_PATH}
-cp {AnalyticStarter,analytic-server.config} ${STARTER_PATH}
+cp AnalyticStarter analytic-server.config analytic-runner.config ${STARTER_PATH}
 echo -e "${color_green}Done. Analytic Starter installed at $STARTER_PATH.${color_reset}"
 
 # Installing AnalyticRunner
 echo -e "${color_green}*Building Analytic Runner...${color_reset}"
 cd ../../AnalyticRunner/Release/
 # make clean && make all
-make
+make all
 RUNNER_PATH=${INSTALL_PATH%%/}/AnalyticRunner
 mkdir -p ${RUNNER_PATH}
 cp AnalyticRunner ${RUNNER_PATH}
@@ -53,7 +53,7 @@ echo -e "${color_green}Done. Analytic Runner installed at $RUNNER_PATH.${color_r
 echo -e "${color_green}*Setup Web Application...${color_reset}"
 cd ../../OpenCCTVWeb/app/assets/programs/PluginArchiveValidator/Release/
 # make clean && make all
-make
+make all
 cd ../../../../../..
 cp -a OpenCCTVWeb/ ${INSTALL_PATH}
 echo -e "${color_green}Done. OpenCCTV Web Application setup at $RUNNER_PATH.${color_reset}"
@@ -63,3 +63,5 @@ ANALYTICS_PATH=${INSTALL_PATH%%/}/analytics
 mkdir -p ${ANALYTICS_PATH}
 CONNECTORS_PATH=${INSTALL_PATH%%/}/vms_connectors
 mkdir -p ${CONNECTORS_PATH}
+
+echo -e "${color_green}OpenCCTV installation Done.${color_reset}"
